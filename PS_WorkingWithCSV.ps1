abfb3b06b461a7e2b909821cfb0c994797d3f256
Sagar Hande <sagarvhande@gmail.com>
2020-12-22 13:51:08 +0530

    topics added

--- PS_WorkingWithCSV.ps1
+++ PS_WorkingWithCSV.ps1
@@ -3,11 +3,11 @@
 Get-Help *csv*
 
 #Import content from Csv
-$listOfRows = Import-Csv .\example.csv
+$listOfRows = Import-Csv 'E:\Data\Contrator.csv'
 $listOfRows
 
 #Import from CSV and cast properties
-$listOfRows = Import-Csv .\example.csv
+$listOfRows = Import-Csv 'E:\Data\samples\import_csv.csv'
 $listOfRows | ForEach-Object {
 #Cast properties
 $_.DateTime = [datetime]$_.DateTime
@@ -17,26 +17,26 @@ $_
 }
 
 #Change property names in an imported object
-Start-Job -ScriptBlock { Get-Process } | Export-Csv -Path .\Jobs.csv -NoTypeInformation
+Start-Job -ScriptBlock { Get-Process } | Export-Csv -Path 'E:\Data\samples\Jobs.csv' -NoTypeInformation
 $Header = 'State', 'MoreData', 'StatusMessage', 'Location', 'Command', 'StateInfo', 'Finished', 'InstanceId', 'Id', 'Name', 'ChildJobs', 'BeginTime', 'EndTime', 'JobType', 'Output', 'Error', 'Progress', 'Verbose', 'Debug', 'Warning', 'Information'
 # Delete the default header from file
-$A = Get-Content -Path .\Jobs.csv
+$A = Get-Content -Path 'E:\Data\samples\Jobs.csv'
 $A = $A[1..($A.Count - 1)]
-$A | Out-File -FilePath .\Jobs.csv
-$J = Import-Csv -Path .\Jobs.csv -Header $Header
+$A | Out-File -FilePath 'E:\Data\samples\Jobs.csv'
+$J = Import-Csv -Path 'E:\Data\samples\Jobs.csv' -Header $Header
 $J
 
 #Exporting Objects as CSV
-Get-Process -Name WmiPrvSE | Select-Object -Property BasePriority,Id,SessionId,WorkingSet |
-Export-Csv -Path .\WmiData.csv -NoTypeInformation
+Get-Process -Name AuthManSvr | Select-Object -Property BasePriority,Id,SessionId,WorkingSet |
+Export-Csv -Path 'E:\Data\samples\WmiData.csv' -NoTypeInformation
 
-Get-Process | Export-Csv -Path .\Processes.csv -Delimiter ';' -NoTypeInformation
+Get-Process | Export-Csv -Path 'E:\Data\samples\WmiData1.csv' -Delimiter ';' -NoTypeInformation
 
 
 #Conversion
 #ConvertTo-Csv
-Get-Process -Name pwsh | ConvertTo-Csv -NoTypeInformation
+Get-Process -Name armsvc | ConvertTo-Csv -NoTypeInformation
 
 #ConvertFrom-Csv
-$P = Get-Process | ConvertTo-Csv
+$P = Get-Process -Name armsvc| ConvertTo-Csv
 $P | ConvertFrom-Csv
---
+++ PowerShell Scripts/Change-File-Extension.ps1
@@ -0,0 +1,11 @@
+﻿#Rename File Extension
+param($Folder)
+
+Write-Host "Before Renaming files...`n`n"
+Get-ChildItem $Folder
+
+Get-ChildItem "$Folder\*.txt" | 
+Rename-Item -NewName { $_.name -Replace '.txt','.bak' }
+
+Write-Host "`n`nAfter Renaming files...`n`n"
+Get-ChildItem $Folder
